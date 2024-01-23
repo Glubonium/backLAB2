07aff243c6a217f0ab5697852a019d32ca339af9
Glubonium <vladzag2004@gmail.com>
2024-01-23 14:34:51 +0200

    add config

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 app = Flask(__name__)
 
-# app.config.from_pyfile('config.py', silent=True)
+app.config.from_pyfile('config.py', silent=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:aot1@localhost/L3"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
